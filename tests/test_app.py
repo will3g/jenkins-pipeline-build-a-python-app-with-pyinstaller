e96e956175a6a722074969a388f60a84e18896db
will3g <willianpaulo_09@hotmail.com>
2022-05-07 14:36:28 -0300

    test-jenkins

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,5 @@
-import os
-import sys
-# jenkins exposes the workspace directory through env.
-
 from src.app import index
 
-sys.path.append(os.environ['WORKSPACE'])
 
 def test_index():
     assert index() == 'Hello, world!'
